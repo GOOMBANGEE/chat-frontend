e1a1cf72260d247d95bcf3ded45ffff60f7a1892
GOOMBANGEE <goom@goomba.org>
2024-08-26 00:57:15 +0900

    refactor: ErrorPage

--- src/page/ErrorPage.tsx
+++ src/page/ErrorPage.tsx
@@ -16,13 +16,13 @@ export default function ErrorPage() {
     navigate(-1);
   };
 
-  return (
-    <div className={"mt-14 px-4 text-white opacity-90"}>
-      <div className={"px-2"}>
-        <div className={"mb-2 font-semibold"}>ERROR 400</div>
-        <div className={"mb-4 text-4xl font-semibold"}>오류</div>
-        {globalState.pageInvalid ? (
-          <>
+  const renderPage = () => {
+    if (globalState.pageInvalid) {
+      return (
+        <div className={"mt-14 px-4 text-white opacity-90"}>
+          <div className={"px-2"}>
+            <div className={"mb-2 font-semibold"}>ERROR 400</div>
+            <div className={"mb-4 text-4xl font-semibold"}>오류</div>
             <div>올바르지않은 주소입니다</div>
             <button
               className={
@@ -34,41 +34,55 @@ export default function ErrorPage() {
             >
               HOME PAGE
             </button>
-          </>
-        ) : (
-          <>
-            {userState.username && !globalState.loginExpire ? (
-              <>
-                <div>{globalState.errorMessage}</div>
-                <button
-                  className={
-                    "mt-12 text-lg text-orange-600 underline underline-offset-8"
-                  }
-                  onClick={() => {
-                    handlePrevious();
-                  }}
-                >
-                  PREVIOUS PAGE
-                </button>
-              </>
-            ) : (
-              <>
-                <div>로그인이 필요합니다</div>
-                <button
-                  className={
-                    "mt-12 text-lg text-orange-600 underline underline-offset-8"
-                  }
-                  onClick={() => {
-                    handleHomePage();
-                  }}
-                >
-                  HOME PAGE
-                </button>
-              </>
-            )}
-          </>
-        )}
+          </div>
+        </div>
+      );
+    }
+
+    if (userState.username && !globalState.loginExpire) {
+      return (
+        <div className={"mt-14 px-4 text-white opacity-90"}>
+          <div className={"px-2"}>
+            <div className={"mb-2 font-semibold"}>ERROR 400</div>
+            <div className={"mb-4 text-4xl font-semibold"}>오류</div>
+
+            <div>{globalState.errorMessage}</div>
+            <button
+              className={
+                "mt-12 text-lg text-orange-600 underline underline-offset-8"
+              }
+              onClick={() => {
+                handlePrevious();
+              }}
+            >
+              PREVIOUS PAGE
+            </button>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className={"mt-14 px-4 text-white opacity-90"}>
+        <div className={"px-2"}>
+          <div className={"mb-2 font-semibold"}>ERROR 400</div>
+          <div className={"mb-4 text-4xl font-semibold"}>오류</div>
+
+          <div>로그인이 필요합니다</div>
+          <button
+            className={
+              "mt-12 text-lg text-orange-600 underline underline-offset-8"
+            }
+            onClick={() => {
+              handleHomePage();
+            }}
+          >
+            HOME PAGE
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  return renderPage();
 }
