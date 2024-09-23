72811308ddda9cc868400afeef3afd6edbc54be0
GOOMBANGEE <goom@goomba.org>
2024-09-23 17:32:27 +0900

    update: devLog stack trace

--- src/devLog.ts
+++ src/devLog.ts
@@ -2,6 +2,8 @@ const ENV = import.meta.env.VITE_ENV;
 
 export default function devLog(file: string, message: string) {
   if (ENV === "dev") {
-    console.log(`[${file}] : ${message}`);
+    const error = new Error(); // 스택 트레이스를 생성
+    const stack = error.stack?.split("\n")[2]?.trim(); // 호출 스택의 세 번째 줄을 가져옴
+    console.log(`[${file}] : ${message}\n${stack}`);
   }
 }
