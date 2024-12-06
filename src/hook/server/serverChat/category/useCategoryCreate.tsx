026425aff461ca4700351f0e8d7ebf107e0ad01b
GOOMBANGEE <goom@goomba.org>
2024-12-06 13:09:19 +0900

    chore: category error message

--- src/hook/server/serverChat/category/useCategoryCreate.tsx
+++ src/hook/server/serverChat/category/useCategoryCreate.tsx
@@ -1,8 +1,9 @@
 import { useServerStore } from "../../../../store/ServerStore.tsx";
 import { useCategoryStore } from "../../../../store/CategoryStore.tsx";
 import { useEnvStore } from "../../../../store/EnvStore.tsx";
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 import devLog from "../../../../devLog.ts";
+import { toast } from "react-toastify";
 
 export default function useCategoryCreate() {
   const { serverState } = useServerStore();
@@ -12,18 +13,23 @@ export default function useCategoryCreate() {
 
   const categoryCreate = async () => {
     const categoryUrl = envState.categoryUrl;
-
-    await axios.post(`${categoryUrl}/${serverState.id}/create`, {
-      name: categoryState.createModalName,
-      // allowRoleIdList: ,
-      // allowUserIdList: ,
-    });
-    devLog(componentName, "setCategoryState createModal false");
-    setCategoryState({
-      createModalOpen: false,
-      createModalName: undefined,
-      createModalOptionOpen: false,
-    });
+    try {
+      await axios.post(`${categoryUrl}/${serverState.id}/create`, {
+        name: categoryState.createModalName,
+        // allowRoleIdList: ,
+        // allowUserIdList: ,
+      });
+      devLog(componentName, "setCategoryState createModal false");
+      setCategoryState({
+        createModalOpen: false,
+        createModalName: undefined,
+        createModalOptionOpen: false,
+      });
+    } catch (error) {
+      if (isAxiosError(error)) {
+        toast.error(error.response?.data.message);
+      }
+    }
   };
 
   return { categoryCreate };
